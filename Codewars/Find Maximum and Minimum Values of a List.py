9b9056e72657670081859f361757c472687487cc
IlyaRice <136918723+IlyaRice@users.noreply.github.com>
2024-03-15 15:32:20 +0700

    Update Find Maximum and Minimum Values of a List.py

--- Codewars/Find Maximum and Minimum Values of a List.py
+++ Codewars/Find Maximum and Minimum Values of a List.py
@@ -1,4 +1,6 @@
-"""Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+"""
+Find Maximum and Minimum Values of a List
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 
 Examples (Input -> Output)
 * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
@@ -6,8 +8,8 @@ Examples (Input -> Output)
 * [42, 54, 65, 87, 0]             -> min = 0, max = 87
 * [5]                             -> min = 5, max = 5
 Notes
-You may consider that there will not be any empty arrays/vectors."""
-
+You may consider that there will not be any empty arrays/vectors.
+"""
 
 
 #Solution
