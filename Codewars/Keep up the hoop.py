9de8edc2b680b4c53e9ad3f841e79958a64919ed
Ilya Rice <79250515615@yandex.com>
2024-04-18 14:38:33 +0700

    Kata solved

---
+++ Codewars/Keep up the hoop.py
@@ -0,0 +1,22 @@
+"""
+Keep up the hoop
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+"""
+
+
+#Solution
+
+def hoop_count(n):
+    return ("Great, now move on to tricks" if n>=10 else "Keep at it until you get it")
+
+#Clever solution
+
+def hoopCount(n):
+    return ("Great, now move on to tricks", "Keep at it until you get it")[n < 10]
+
+# Uses tuple indexing with a Boolean to select the return message based on whether n is less than 10.
